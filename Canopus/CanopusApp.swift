35903b3c4db0dfa2fdb09eff65793c710860058e
Alex <oderiugin@gmail.com>
2026-09-02 23:07:10 -0500

    Initial Commit

---
+++ Canopus/CanopusApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanopusApp.swift
+//  Canopus
+//
+//  Created by Oleksandr Deriugin on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct CanopusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
